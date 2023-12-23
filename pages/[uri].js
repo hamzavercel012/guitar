8446d10f97a9b2d234850ae35816f9b7ea935bdc
hamzavercel012 <123652200+hamzavercel012@users.noreply.github.com>
2023-12-23 18:49:03 +0500

    Update [uri].js

--- pages/[uri].js
+++ pages/[uri].js
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 
 export default function SlugPage({ post }) {
 useEffect(()=>{
-  location = `https://wildcaten.com/${post?.slug}`
+  location = `https://guitarhere.com/${post?.slug}`
 },[])
   return (
     <div>
